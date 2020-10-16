0bdfc9225676d4a96f59c8ad2a1ba395eb6a091e
HeszMonika <heszmoni2001@gmail.com>
2020-10-16 10:12:37 +0100

    Add project files.

---
+++ balkezesek/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace balkezesek
+{
+    class Program
+    {
+        static List<balkezesek> balkezes = new List<balkezesek>();
+
+
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("balkezesek.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                balkezes.Add(new balkezesek(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            foreach (var b in balkezes)
+            {
+                Console.WriteLine($"{b.nev, 20} {b.elso} {b.utolso} {b.suly} {b.magassag}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ balkezesek/balkezesek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    class balkezesek
+    {
+        public string nev { get; private set; }
+        public string elso { get; private set; }
+        public string utolso { get; private set; }
+        public int suly { get; private set; }
+        public int magassag { get; private set; }
+
+        public balkezesek(string sor)
+        {
+            string[] a= sor.Split(';');
+            nev = a[0];
+            elso = a[1];
+            utolso = a[2];
+            suly = Convert.ToInt32(a[3]);
+            magassag = Convert.ToInt32(a[4]);
+        }
+    }
+}
